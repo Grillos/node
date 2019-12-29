787cd26d5038dc20166df154dfb13f4835fa70b7
Robson Silva Grillo <robson.grillo@hdi.com.br>
2019-12-29 01:52:24 -0300

    feat(devops): Helm chart kubernetes

--- account/server.js
+++ account/server.js
@@ -1,5 +1,9 @@
 const app = require('./src/config/custom-express')
 
+app.get('/', function (req, res) {
+    res.send('Account up and running!');
+  });
+
 app.listen(3000, function(){
     console.log('Server running in port 3000')
 })
--- account/src/config/mongoose-conect.js
+++ account/src/config/mongoose-conect.js
@@ -1,6 +1,6 @@
 var restful = require('node-restful'),
     mongoose = restful.mongoose;
     
-mongoose.connect('mongodb://db/account');
+mongoose.connect(process.env.VORTICS_MONGODB_URI || 'mongodb+srv://vortics:rdgpsfsWjyqEBxCv@cluster0-aei21.gcp.mongodb.net/vortics-db?retryWrites=true&w=majority');
 
 module.exports = mongoose
