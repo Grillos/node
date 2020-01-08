bfca458eba14bcef82705efe6c15a2166ef8bb95
Grillos <robsongrillo13@gmail.com>
2020-01-07 22:27:04 -0300

    fix(mongo): remove URI

--- account/src/config/mongoose-conect.js
+++ account/src/config/mongoose-conect.js
@@ -1,6 +1,6 @@
 var restful = require('node-restful'),
     mongoose = restful.mongoose;
     
-mongoose.connect(process.env.VORTICS_MONGODB_URI || 'mongodb+srv://vortics:rdgpsfsWjyqEBxCv@cluster0-aei21.gcp.mongodb.net/vortics-db?retryWrites=true&w=majority');
+mongoose.connect(process.env.VORTICS_MONGODB_URI || '');
 
 module.exports = mongoose
